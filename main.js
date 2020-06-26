53f8dad66b2fc4a84409ee41a1bdc6a37fd1a650
valplusplusle <v.kummert@drmnt.de>
2020-06-27 01:34:23 +0200

    build first version, add appimage to project

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow () {
   mainWindow.loadFile('./index.html')
   mainWindow.removeMenu()
   // Open the DevTools.
-   mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
